e3920a744a0ca165a593b9e3494ae134c7e075a1
asiddiqa249 <asiddiqa249@gmail.com>
2023-10-17 20:43:54 +0530

    search & strmethods

---
+++ js task7-str methods/index.js
@@ -0,0 +1,65 @@
+// 1***********Mobile no**************
+// num = +prompt("enter num");
+// if (num.length === 10) {
+//     if (num.startsWith(9 || 8 || 7 || 6)) {
+//         console.log("indian number");
+//     } else {
+//         console.log("not a indian number");
+//     }
+// } else {
+//     console.log("not a mobile number");
+// }
+
+// 2****************removing space*************
+// str1 = "  aye  sh a";
+// str2 = "";
+// for (i = 0; i < str1.length; i++){
+//     if (str1[i] != " ") {
+//         str2 += str1[i];
+//     }
+// }
+// console.log(str2);
+
+// 3*****************removing @*********************
+// str1 = "1@23@43@56@778@";
+// str2 = "";
+// for (i = 0; i < str1.length; i++){
+//     if (str1[i] != "@") {
+//         str2 += str1[i];
+//     }
+// }
+// console.log(str2);
+
+// 4****************removing even index************
+// str1 = "hello guys";
+// str2 = "";
+// for (i = 0; i < str1.length; i++){
+//     if (i%2 !== 0) {
+//         str2 += str1[i];
+//     }
+// }
+// console.log(str2);
+
+// 5*************counting 'a' in a string**************
+// str = "ayesha siddiqa";
+// count = 0;
+// for (i = 0; i < str.length; i++){
+//     if (str[i] == "a") {
+//         count++;
+//     }
+// }
+// console.log(count);
+
+// 6**************gmail****************
+// str = prompt("enter str");
+// if (str.endsWith("@gmail.com")) {
+//     console.log("valid mail");
+// } else {
+//     console.log("invalid mail");
+// }
+
+// if (str.substr(-10) == "@gmail.com") {
+//   console.log("valid mail");
+// } else {
+//   console.log("invalid mail");
+// }
