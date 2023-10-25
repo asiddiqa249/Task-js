d2ada8ec118d6522d5ad88732d8dd33bd4f27d94
asiddiqa249 <asiddiqa249@gmail.com>
2023-10-25 20:03:34 +0530

    js task

---
+++ js task8-array/index.js
@@ -0,0 +1,54 @@
+//1***************taking even index out**********************
+// arr = ["ayesha", 2, "siddiqa", 4, 5, "mohammad"];
+// for (i = 0; i < arr.length;i++) {
+//     if (i % 2==0) {
+//         console.log(arr[i]);
+//     }
+// }
+
+
+
+// 2*****************length of names in array****************
+// arr1 = ["ayesha", "siddiqa", "sadiya", "umar", "siblings"];
+// for (i = 0; i < arr1.length; i++){
+//     console.log(arr1[i].length);
+// }
+
+// arr1 = ["ayesha", "siddiqa", "sadiya", "umar", "siblings"];
+
+// for (i = 0; i < arr1.length; i++){
+//     c = 0;
+//     for (j = 0; j < arr1[i].length; j++) {
+//         c++;
+//     }console.log(c);
+// }
+
+// 3****Print vowels of names in array***********************
+// arr1 = ["ayesha", "siddiqa", "sadiya", "umar", "siblings"];
+// c = [];
+// count=0
+// for (i = 0; i < arr1.length; i++){
+//     arr2 = arr1[i];
+//     for (j = 0; j < arr2.length; j++){
+//         if (arr2[j] == "a" || arr2[j] == "e" || arr2[j] == "i" || arr2[j] == "o" || arr2[j] == "u") {
+//             c += arr2[j];
+//             count++;
+//         }
+//     }
+//     console.log(c);
+//     console.log(count);
+// }
+
+// Array pop()
+// arr = ["ayesha", "siddiqa", "sadiya", "umar"];
+// console.log(arr.pop());
+// console.log(arr.push("kavya"));
+// console.log(arr.join("@"));
+// b=arr.shift();
+// console.log(b);
+
+
+// let a = {
+//     skill:"css", head:"html"
+// }
+// console.log(Object.values(a).length);
